0b53f158315b2fb375c898861b6fecd34745291b
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-08-17 16:48:53 +0530

    pract: Variables basics

--- helloworld.js
+++ helloworld.js
@@ -1,3 +1,19 @@
 console.log("helloworld");
 
-alert("hello");
+// alert("hello world");
+
+// this is a comment
+
+// variables
+var st = "hey world";
+console.log(st);
+
+var someNumber = 47;
+console.log(someNumber);
+
+// document.getElementById("someText").innerHTML = st;
+
+var age = prompt("what is your age?");
+document.getElementById("someText").innerHTML = age;
+
+//NUMBERS
